de72a6905c058bfb8a6b4b3655e3ad01b609f46c
Pranshu Sinha <pranshu.qwerty06@gmail.com>
2025-11-12 18:38:12 +0530

    set render base url

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
-export const BASE_URL = "http://localhost:8080";
+// export const BASE_URL = "http://localhost:8080";
 
 // export const BASE_URL = "http://blog-app-env.eba-jprayzxb.ap-south-1.elasticbeanstalk.com";
+export const BASE_URL = "https://cricket-blog-app-backend.onrender.com";
 
 export const myAxios = axios.create({
     baseURL: BASE_URL
